06cdba7fec29d1b4d87de42f03ccac92a0773ba6
SamEThibault <sam5thibault@gmail.com>
2022-11-15 10:55:48 -0500

    documentation and script added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# neopixel-shiftlight
-Formula-style shift light built for the Queen's Racing team using RGB Neopixel Strip powered by Arduino R3.
+# NeoPixels Shiftlight
+Formula-style shift light built for the Queen's Racing team using Adafruit RGB NeoPixels Strip powered by Arduino R3 microcontroller.
+
+The file is mostly modular and includes a quick starting animation. Dependencies required include the Adafruit NeoPixels library.
+
+For the shift light to work for any application, users must specify the controller data pin used, the number of LEDs in the strip, the brightness (0-255), the engine speed's redline, and the ideal shift range in the initial `define` statements.
+
+This application will be integrated with the car's CAN bus to receive current RPM values. For now, test values are used for demonstration purposes.
---
+++ qfsae_rpm1.ino
@@ -0,0 +1,147 @@
+/**
+  Organization: Queen's Racing, 2022
+  Program:      NeoPixel RPM Light Test
+  Author:       Samuel Emard-Thibault
+  Version:      Revision 1
+  Date:         11/14/2022
+**/
+
+#include <Adafruit_NeoPixel.h>
+
+#define BRIGHTNESS 150 // 0 to 255
+#define PIN      6    // digital Arduino pin
+#define N_LEDS 16     // number of pixels on strip
+#define REDLINE 7500  // define the engine speed's limit
+
+int idealShiftSpeed = REDLINE - 300;  // generalized, find real ideal speed using actual HP/TQ curves
+
+// initialize strip object and colors
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+uint16_t green = strip.Color(0, BRIGHTNESS, 0);
+uint16_t red = strip.Color(BRIGHTNESS, 0, 0);
+uint16_t blue = strip.Color(0, 0, BRIGHTNESS);
+
+// test values
+float currEngineSpeed[11] = {1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0, 7300.0, 7400.0, 7450.0, 7500.0};
+
+void setup() {
+  strip.begin();
+  startAnimation();
+}
+
+void loop() {
+
+  // for each RPM input (test data)
+  for (int i = 0; i < 11; i++)
+  {
+    // if we're at or past the ideal engine speed for upshifts, flash
+    if (currEngineSpeed[i] >= idealShiftSpeed)
+    {
+      flash();
+    } else
+    {
+      // else, find % of current engine speed, and map it to the percentage of pixels we want to light up
+      int numPixels = (currEngineSpeed[i] / REDLINE) * N_LEDS;
+      updateLEDS(numPixels);
+    }
+    delay(50);
+  }
+}
+
+// flash full blue strip once
+void flash()
+{
+  strip.clear();
+  strip.show();
+  delay(30);
+  for (int i = 0; i < N_LEDS; i++)
+    strip.setPixelColor(i, blue);
+  strip.show();
+}
+
+// updates the strip based on current numLEDS calculated in loop()
+void updateLEDS(int n)
+{
+  // clear the strip (in case engine speed decreased)
+  strip.clear();
+
+  // light up number of pixels based on parameter
+  for (int i = 0; i < n; i++)
+  {
+    // if we're at least 60% to the redline, light up blue, else light up green
+    if (i < N_LEDS * 0.6)
+    {
+      strip.setPixelColor(i, green);
+    } else
+    {
+      strip.setPixelColor(i, blue);
+    }
+  }
+  strip.show();
+}
+
+// starting animation (~2sec)
+void startAnimation()
+{
+
+  // collision animation
+  for (int i = 0; i < N_LEDS; i++)
+  {
+    strip.setPixelColor(i, blue);
+    strip.setPixelColor(N_LEDS - i, blue);
+    strip.show();
+    delay(25);
+  }
+  for (int i = 0; i < N_LEDS; i++)
+  {
+    strip.setPixelColor(i, 0);
+    strip.setPixelColor(N_LEDS - i, 0);
+    strip.show();
+    delay(25);
+  }
+
+  // chase animation
+  for (int i = 0; i < N_LEDS + 4; i++)
+  {
+    strip.setPixelColor(i, green);
+
+    if (i > 3)
+      strip.setPixelColor(i-4, 0);
+
+    strip.show();
+    delay(15);
+  }
+  for (int i = N_LEDS; i > -5; i--)
+  {
+    strip.setPixelColor(i, green);
+
+    if (i < N_LEDS)
+      strip.setPixelColor(i+4, 0);
+
+    strip.show();
+    delay(15);
+  }
+
+  // glowing animation
+  int intensity = 0;
+  while (intensity < BRIGHTNESS)
+  {
+    for (int i = 0; i < N_LEDS; i++)
+    {
+      strip.setPixelColor(i, strip.Color(0, 0, intensity));
+    }
+    strip.show();
+    delay(2);
+    intensity++;
+  }
+  while (intensity >= 0)
+  {
+    for (int i = 0; i < N_LEDS; i++)
+    {
+      strip.setPixelColor(i, strip.Color(0, 0, intensity));
+    }
+    strip.show();
+    delay(2);
+    intensity--;
+  }
+}
